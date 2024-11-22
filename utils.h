05747a12247d44f548372547ddd86374fc728e09
jakob.nyx <jakob.niessner@h-its.org>
2024-11-22 11:53:24 +0100

    Before workstation migration

---
+++ utils.h
@@ -0,0 +1,28 @@
+#include<tuple>
+#include<utility>
+#include<iostream>
+#include<vector>
+#include<array>
+
+namespace utils{
+
+  template<typename Arg1, typename... Args>
+  Arg1&& get_first(Arg1&& arg1, Args&&...){
+	return std::forward<Arg1>(arg1);
+  }
+
+
+  template<typename Arg1, typename... Args>
+  auto get_tail(Arg1&& arg1, Args... args){
+	auto tail = std::forward_as_tuple(std::forward<Args>(args)...);
+       	return tail;	
+  }
+
+  template<typename Tuple, std::size_t... Ints>
+  std::tuple<std::tuple_element_t<Ints, Tuple>...>
+  extract_tuple(Tuple&& tuple, std::index_sequence<Ints...>) {
+   return { std::get<Ints>(std::forward<Tuple>(tuple))... };
+  }
+
+}
+
