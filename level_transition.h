0da82e4b339f50df94fa980fe7f1eaabc111a9a7
jakob.nyx <jakob.niessner@h-its.org>
2024-11-22 11:50:22 +0100

    Before mogration to workstation

---
+++ level_transition.h
@@ -0,0 +1,141 @@
+#include "CG_Solver.h"
+#include "Convolution.h"
+#include "utils.h"
+
+#ifndef LEVEL_TRANSITION
+#define LEVEL_TRANSITION
+
+std::string tuple_type;
+
+template<typename T> struct TD;
+
+template<typename DataType, typename Offsets, size_t size, Dimension Dim, Length... strides_all, std::size_t... dims>
+void coarsening(Domain<Dim, ((strides_all+1)/2-1)...>& dest, Domain<Dim, strides_all...>& src, const std::array<DataType, size> values, const std::array<Offsets, size> offsets, std::index_sequence<dims...>)
+{
+	assert(dest.q == src.q);
+	//assert(dest.padding_width == src.padding_width);
+
+	dest.q.submit([&](sycl::handler& h){
+		h.parallel_for(sycl::range<Dim>(dest.strides[dims]...), [=](sycl::id<Dim> I){
+				
+				((I[dims] += dest.padding_width), ...);
+				sycl::id<Dim> I_fine;
+				((I_fine[dims] = 2*I[dims]),...);
+
+				float result = 0;
+
+				for(int k = 0; k< size; k++){
+					result += src((I_fine[dims]+offsets[k][dims])...)*values[k];
+				}
+
+				dest(I[dims]...) = result;
+		});
+	});
+
+	dest.q.wait();
+}
+
+template<typename DataType, typename Offsets, size_t size, Dimension Dim, Length... strides_all>
+void coarsening(Domain<Dim, ((strides_all+1)/2-1)...>& dest, Domain<Dim, strides_all...>& src, const std::array<DataType, size> values, const std::array<Offsets, size> offsets)
+{
+	coarsening(dest, src, values, offsets, std::make_index_sequence<Dim>());
+}
+
+
+
+template<Dimension Dim, Length... strides_all, std::size_t... dims>
+void refinement(Domain<Dim, strides_all...>& dest, Domain<Dim, ((strides_all+1)/2-1)...>& src, std::index_sequence<dims...>)
+{
+	assert(dest.q == src.q);
+	//assert(dest.padding_width == src.padding_width);
+
+	dest.q.submit([&](sycl::handler& h){
+		h.parallel_for(sycl::range<Dim>(dest.strides[dims]...), [=](sycl::id<Dim> I){
+				
+				((I[dims] += dest.padding_width), ...);
+				std::tuple<> empty_index_tuple;
+				dest(I[dims]...) = domain_refinement_helper(src, empty_index_tuple, I[dims]...);
+		});
+	});
+
+	dest.q.wait();
+
+	std::cout<<"The tuple type is: "<<tuple_type<<std::endl;
+}
+
+template<Dimension Dim, Length...strides_all, typename... Index, typename... Rest_indices>
+DataType domain_refinement_helper(Domain<Dim, strides_all...> dom, std::tuple<Index...>& index_tuple, Rest_indices... rest_indices)
+{
+	if constexpr (sizeof...(Index) == Dim)
+	{
+		for(int i = 0; i < Dim; i++)
+		{
+			std::get<i>(index_tuple) /= 2;
+		}	
+		return std::apply(dom, index_tuple);
+	}
+	else
+	{
+		auto first = utils::get_first(rest_indices...);	
+		auto rest  = utils::get_tail(rest_indices...);
+
+		if (first % 2 == 0)
+		{
+
+			auto new_index_tuple = std::tuple_cat(index_tuple, std::make_tuple(first));
+			
+			auto first_arg_tuple = std::forward_as_tuple(
+				       std::forward<Domain<Dim, strides_all...>>(dom), 
+				       std::forward<decltype(new_index_tuple)>(new_index_tuple));
+			
+			auto arg_tuple = std::tuple_cat(std::forward<decltype(first_arg_tuple)>(first_arg_tuple),
+							std::forward<decltype(rest)>(rest));
+
+			//TD<decltype(arg_tuple)> td;
+			
+			std::apply(
+   				 [](auto&&... args) {
+        				return domain_refinement_helper<decltype(args)...>(std::forward<decltype(args)>(args)...);
+    				}, arg_tuple);
+			
+			//return std::apply(domain_refinement_helper<decltype(dom),
+			//					   decltype(new_index_tuple),
+			//					   unsigned long>, arg_tuple);
+
+		}
+//		else
+//		{	
+//			auto new_index_tuple_1 = std::tuple_cat(index_tuple, std::make_tuple(first+1));
+//			auto new_index_tuple_2 = std::tuple_cat(index_tuple, std::make_tuple(first-1));
+//
+//			auto first_arg_tuple_1 = std::forward_as_tuple(
+//				       std::forward<Domain<Dim, strides_all...>>(dom), 
+//				       std::forward<decltype(new_index_tuple_1)>(new_index_tuple_1));
+//
+//			auto first_arg_tuple_2 = std::forward_as_tuple(
+//				       std::forward<Domain<Dim, strides_all...>>(dom), 
+//				       std::forward<decltype(new_index_tuple_2)>(new_index_tuple_2));
+//
+//			auto arg_tuple_1 = std::tuple_cat(std::forward<decltype(first_arg_tuple_1)> 
+//								(first_arg_tuple_1), rest);
+//
+//			auto arg_tuple_2 = std::tuple_cat(std::forward<decltype(first_arg_tuple_2)>
+//                                                                (first_arg_tuple_2), rest);
+//
+//			//return (std::apply(domain_refinement_helper, arg_tuple_1)+
+//			//	std::apply(domain_refinement_helper, arg_tuple_2))/2;
+//		}
+		
+	}
+	
+	return 0.;
+	
+}
+
+template<Dimension Dim, Length... strides_all>
+void refinement(Domain<Dim, strides_all...>& dest, Domain<Dim, ((strides_all+1)/2-1)...>& src)
+{
+	refinement(dest, src, std::make_index_sequence<Dim>());
+}
+
+#endif
---
+++ tests_level_transition.cc
@@ -0,0 +1,60 @@
+#include "level_transition.h"
+#include "Convolution.h"
+#include "CG_Solver.h"
+
+int main()
+{
+	sycl::cpu_selector selector;
+	sycl::queue q(selector, sycl::property_list{sycl::property::queue::in_order{}});
+
+	Domain<2, 3u, 3u> domain_dest(Paddings::PERIODIC, q, 1);
+	Domain<2, 7u, 7u> domain_src(Paddings::PERIODIC, q, 1);
+
+	constexpr std::array<DataType, 9> values{1./9, 1./9, 1/9., 1./9., 1./9, 1./9, 1./9, 1./9, 1./9};
+	constexpr std::array<OffsetType, 9> offsets{{{0, 0}, {1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1,1}, {-1, 1}, {1, -1}, {-1, -1}}};
+
+	//====== First Trial ======//
+	std::cout<<"The input matrix is given by: "<<std::endl;
+ 	for(Position1D i = 1; i<7+1; i++)
+ 	{
+ 		for(Position1D j = 1; j<7+1; j++)
+ 		{
+ 			domain_src.set_value(((i+j)%2==0)*1,  i, j);
+ 		}
+ 
+ 		std::cout<<std::endl;
+ 	}
+
+	for(Position1D i = 0; i<7+2; i++)
+ 	{
+ 		for(Position1D j = 0; j<7+2; j++)
+ 		{
+ 			//domain_src.set_value(1,  i, j);
+ 			std::cout<< domain_src.get_value(i, j) << " ";
+ 		}
+ 
+ 		std::cout<<std::endl;
+ 	}
+
+	coarsening(domain_dest, domain_src, values, offsets);
+
+	std::cout<<std::endl;
+
+	for(Position1D i = 0; i<3+2; i++)
+        {
+                for(Position1D j = 0; j<3+2; j++)
+                {
+                        //domain_src.set_value(1,  i, j);
+                        std::cout<< domain_dest.get_value(i, j) << " ";
+                }
+
+                std::cout<<std::endl;
+        }
+
+	refinement(domain_src, domain_dest);
+
+
+	
+
+
+}
