e5b16c6b09e42fe9af607dbb99593137be3f6149
Jakob Niessner <jakob.niessner@uni-heidelber.de>
2024-12-09 23:28:36 +0100

    Started writing boundary coarsening function

--- Laplacian_2D.cc
+++ Laplacian_2D.cc
@@ -1,5 +1,26 @@
 #include "MultigridDomain.h"
 
+
+//This function will be called inside the sycl::queue. It sets the values at the boundary correctly on the Domain which is passed in by reference
+template<Dimension Dim, std::size_t size ,std::size_t... strides_all, std::size_t... previous, std::size_t current, std::size_t... following>
+void coarsen_boundary_condition(std::array<DataType, Dim-1>& values, 
+                            	std::array<OffsetType, Dim-1>& offsets,
+				sycl::id<Dim-1> I
+                            	Domain<Dim, ((strides_all+1)/2-1)...>& dest, 
+                            	Domain<Dim, strides_all...>& src,
+                            	std::index_sequence<previous...>,  
+                            	std::sequence<following...>)
+{       
+	DataType value = 0;
+	auto index_tuple = std::forward_as_tuple(std::forward(I[previous])...,
+						 current,
+						 std::forward(I[following])...);
+
+	//for(std::size_t i = 0; i<
+}
+
+
+
 int main()
 {
 	sycl::gpu_selector selector;
