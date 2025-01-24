883687406ccd25b1e67abc911c9278557ad6e3e0
Jakob Niessner <jakob.niessner@uni-heidelber.de>
2025-01-24 22:59:20 +0100

    Added debugging directory and started debugging charge_distribution maker, which currently can not e offloaded to the device without segfault

--- create_charge_distribution.cc
+++ create_charge_distribution.cc
@@ -17,7 +17,7 @@ int main(int argc, char *argv[]) {
               << std::endl;
   }
 
-  constexpr std::size_t length = 3;
+  constexpr std::size_t length = 95;
 
   sycl::cpu_selector selector;
   sycl::queue q{selector,
@@ -54,11 +54,16 @@ int main(int argc, char *argv[]) {
   domain::Domain<3, length, length, length> domain(Paddings::PERIODIC, q, 1);
   q.wait();
 
-  q.parallel_for(sycl::range<1>(atoms.size()), [=](sycl::id<1> I) {
-     add_charges_to_distribution(domain, atoms_device[I].Position,
-                                 atoms_device[I].charge,
-                                 spacing<DataType, 1.>{});
-   }).wait();
+  // q.parallel_for(sycl::range<1>(atoms.size()), [=](sycl::id<1> I) {
+  // q.submit([=](sycl::handler &h) {
+  //   h.single_task([=]() {
+  for (int I = 0; I < atoms.size(); I++)
+    add_charges_to_distribution(domain, atoms_device[I].Position,
+                                atoms_device[I].charge,
+                                spacing<DataType, 1.>{});
+  //   });
+  // }).wait();
+  //}).wait();
 
-  domain.print_dx_to_stream(dx_file, a[0], a[1], a[2], 4.);
+  domain.print_dx_to_stream(dx_file, a[0], a[1], a[2], 96.);
 }
---
+++ test_kappa_function/count_non_zeros.py
@@ -0,0 +1,29 @@
+import numpy as np 
+import sys; sys.path.append('../../interesting_code_snippets/')
+from itertools import chain
+import time
+from functions import read_dx_grid
+import pdb
+
+
+if __name__=="__main__":
+    #  if len(sys.argv)<3:
+    #    print("Usage: python3 this_script in_file1 in_file2")
+    
+    in_file1=sys.argv[1]
+ #   in_file2=sys.argv[2]
+    
+  #  pdb.set_trace()
+    potential_grid1, _, _ = read_dx_grid(in_file1)
+ #   potential_grid2, _, _ = read_dx_grid(in_file2)
+
+    #potential_grid_comparison = np.absolute(potential_grid1-potential_grid2)
+    
+    potential_grid_diff = potential_grid1[potential_grid1>0.0]
+
+    print("The number of points with with different values is: ", 
+          potential_grid_diff.size)
+    
+
+
+
