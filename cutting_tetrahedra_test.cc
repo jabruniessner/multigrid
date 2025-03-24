43953d187f8dfe82e3eed4b48e9f7904528ab3de
Jakob Niessner <jakob.niessner@uni-heidelber.de>
2025-03-24 18:21:07 +0100

    Wrote a generator that allows to iterate over all edge point of the sphere

--- cutting_tetrahedra_test.cc
+++ cutting_tetrahedra_test.cc
@@ -1,36 +1,56 @@
+#include "Atom_types.h"
 #include "Domain.h"
+#include "dot_finder.h"
+#include "hipSYCL/sycl/device_selector.hpp"
 #include "hipSYCL/sycl/queue.hpp"
+#include "hipSYCL/sycl/usm.hpp"
 #include "tetraeda_type.h"
 #include <iostream>
 #include <sycl/sycl.hpp>
 
 int main(int argc, char *argv[]) {
 
-#ifdef DEBUGMODE
-  sycl::cpu_selector selector;
-#else
-  sycl::gpu_selector selector;
-#endif
+  // #ifdef DEBUGMODE
+  //   sycl::cpu_selector selector;
+  // #else
+  //   sycl::gpu_selector selector;
+  // #endif
+  //
+  // Using host device for prove of concept.
 
-  sycl::queue q{selector,
+  sycl::queue q{sycl::host_selector{},
                 sycl::property_list{sycl::property::queue::in_order{}}};
 
   tetraeda_line_points<3> tet_example;
 
-  domain::Grid<tetraeda_line_points<3>, 3, 10, 10, 10> tet_grid(
+  Atom<DataType> atom_host;
+  atom_host.Position[0] = 6;
+  atom_host.Position[1] = 6;
+  atom_host.Position[2] = 6;
+  atom_host.radius = 5;
+
+  // Atom<DataType> *atom_device = sycl::malloc_host<Atom<DataType>>(1, q);
+  // q.memcpy(atom_device, &atom_host, sizeof(Atom<DataType>)).wait();
+  domain::Grid<tetraeda_line_points<3>, 3, 100, 100, 100> tet_grid(
       Paddings::PERIODIC, q, 1);
+  // std::cout << "Hello World!" << std::endl;
+  // using vector3d = vector<DataType, 3>;
+  // vector3d footpoint(0.25f, 0.f, 0.f), center(0.f, 0.f, 0.f),
+  //    direction(1.f, 0.f, 0.f);
+  // DataType isec_p =
+  //    find_intersection_point_sphere<3>(footpoint, direction, center, 1.f);
+  // std::cout << "The intersection distance is " << isec_p << std::endl;
+  //
 
-  std::cout << "Hello World!" << std::endl;
+  constexpr DataType grid_step = 1;
 
-  using vector3d = vector<DataType, 3>;
-
-  vector3d footpoint(0.25f, 0.f, 0.f), center(0.f, 0.f, 0.f),
-      direction(1.f, 0.f, 0.f);
-
-  DataType isec_p =
-      find_intersection_point_sphere<3>(footpoint, direction, center, 1.f);
-
-  std::cout << "The intersection distance is " << isec_p << std::endl;
+  auto edge_points = line_sphere_intersection(atom_host, grid_step);
+  for (auto edge_point : edge_points) {
+    for (auto i : edge_point) {
+      std::cout << i << " ";
+    }
+    std::cout << std::endl;
+  }
 
   return 0;
 }
--- dot_finder.h
+++ dot_finder.h
@@ -1,18 +1,22 @@
 #include "Atom_types.h"
 #include "Domain.h"
+#include "blas.h"
 #include "predefinitions.h"
 #include "utils.h"
 #include <array>
+#include <bits/elements_of.h>
 #include <cstddef>
+#include <generator>
 #include <iostream>
 #include <random>
 #include <sycl/sycl.hpp>
+#include <tuple>
 #include <utility>
 
 #ifndef DOT_FINDER_H
 #define DOT_FINDER_H
 
-template <typename T> inline T square(T x) { return x * x; }
+// template <typename T> inline T square(T x) { return x * x; }
 
 template <typename DataType, Dimension Dim, Length... strides,
           typename... Positions, Length... directions>
@@ -84,6 +88,92 @@ template <std::size_t... numbers>
 void print_index_sequence(std::index_sequence<numbers...>) {
   ((std::cout << numbers << " "), ...);
   std::cout << std::endl;
+}
+
+template <typename DataType, Dimension Dim, typename... Positions,
+          Length... directions>
+std::generator<std::array<int, Dim>> line_sphere_intersections_helper(
+    Sphere<DataType, Dim> &sphere, DataType grid_step,
+    std::index_sequence<directions...>, Positions... positions) {
+
+  static_assert(sizeof...(directions) <= Dim,
+                "Break condition never satisfied");
+
+  if constexpr (sizeof...(directions) == Dim - 1) {
+
+    const auto &Position = sphere.Position;
+    const auto &current_value = Position[sizeof...(directions)];
+    const auto &radius = sphere.radius;
+
+    auto sqrt_squared =
+        (square(radius) -
+         (square(Position[directions] - grid_step * positions) + ...));
+
+    if (sqrt_squared < 0) {
+      sqrt_squared = 0;
+    }
+
+    const auto sqrt = std::sqrt(sqrt_squared);
+
+    const int lower_bound =
+        static_cast<int>(std::ceil((current_value - sqrt) / grid_step));
+
+    co_yield std::array<int, Dim>{{(positions)..., lower_bound}};
+
+    const int upper_bound =
+        static_cast<int>((current_value + sqrt) / grid_step);
+
+    co_yield std::array<int, Dim>{static_cast<int>(positions)..., upper_bound};
+
+  } else if constexpr (sizeof...(directions) == 0) {
+
+    const int lower_bound = static_cast<int>(
+        std::ceil((sphere.Position[0] - sphere.radius) / grid_step));
+    const int upper_bound =
+        static_cast<int>((sphere.Position[0] + sphere.radius) / grid_step);
+
+    for (int i = lower_bound; i <= upper_bound; ++i) {
+      co_yield std::ranges::elements_of(line_sphere_intersections_helper(
+          sphere, grid_step,
+          std::make_index_sequence<sizeof...(directions) + 1>{}, i));
+    }
+
+  } else {
+    const auto &Position = sphere.Position;
+    const auto &current_value = Position[sizeof...(directions)];
+    const auto &radius = sphere.radius;
+
+    auto sqrt_squared =
+        (square(radius) -
+         (square(Position[directions] - grid_step * positions) + ...));
+
+    if (sqrt_squared < 0) {
+      sqrt_squared = 0;
+    }
+
+    const auto sqrt = std::sqrt(sqrt_squared);
+
+    const int lower_bound =
+        static_cast<int>(std::ceil((current_value - sqrt) / grid_step));
+
+    const int upper_bound =
+        static_cast<int>((current_value + sqrt) / grid_step);
+
+    for (int i = lower_bound; i <= upper_bound; i++) {
+      co_yield std::ranges::elements_of(line_sphere_intersections_helper(
+          sphere, grid_step,
+          std::make_index_sequence<sizeof...(directions) + 1>{}, positions...,
+          i));
+    }
+  }
+}
+
+template <typename DataType, Dimension Dim>
+std::generator<std::array<int, Dim>>
+line_sphere_intersection(Sphere<DataType, Dim> &sphere,
+                         const DataType grid_step) {
+  co_yield std::ranges::elements_of(line_sphere_intersections_helper(
+      sphere, grid_step, std::make_index_sequence<0u>{}));
 }
 
 #endif
