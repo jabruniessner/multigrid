3f81bfb996e5177fde67f8382dabe7562cbf23a8
Jakob Niessner <jakob.niessner@uni-heidelber.de>
2025-03-25 02:13:55 +0100

    Started implementing find_polygon_cuts function

--- cutting_tetrahedra_test.cc
+++ cutting_tetrahedra_test.cc
@@ -4,22 +4,49 @@
 #include "hipSYCL/sycl/device_selector.hpp"
 #include "hipSYCL/sycl/queue.hpp"
 #include "tetraeda_type.h"
+#include <bitset>
 #include <iostream>
 #include <sycl/sycl.hpp>
 
 constexpr std::size_t Dim = 3;
 
+// std::countr_zero
+// std::countl_zero
+
 void find_polygon_cuts(std::array<int, Dim> point) {
   // Iteration over all cubes
   for (std::uint8_t i = 0; i < 8; i++) {
 
+    if (i == 0 || i == 7) {
+      // Iteration over all lines
+      for (std::uint8_t j = 1; i < 8; i++) {
+      }
+      // Iteration over all tetrahedra
+      for (std::uint8_t j = 1; i < 6; i++) {
+      }
+    } else if (std::bitset<8>(i).count() == 1) {
+      std::array<std::uint8_t, 2> other_points{
+          static_cast<std::uint8_t>(((i << 1 | i >> (3 - 1)) & ~248) | i),
+          static_cast<std::uint8_t>(((i >> 1 | i << (3 - 1)) & ~248) | i)};
+
+      // Iteration over all lines
+      for (std::uint8_t j = 1; j <= 3; j++) {
+      }
+      // Iteration over all tetrahedra
+      for (std::uint8_t j = 1; j <= 2; j++) {
+      }
+    } else {
+      std::uint8_t i_inv = ~i;
+      std::array<std::uint8_t, 2> other_points{
+          static_cast<std::uint8_t>(((i_inv << 1 | i_inv >> (3 - 1)) & ~248)),
+          static_cast<std::uint8_t>(((i_inv >> 1 | i_inv << (3 - 1)) & ~248))};
+    };
+
     // Iteration over all lines
-    for (std::uint8_t i = 1; i < 8; i++) {
-    }
-    // Iteration over all tetrahedra
-    for (std::uint8_t i = 1; i < 6; i++) {
+    for (std::uint8_t j = 1; j <= 2; j++) {
     }
   }
+}
 }
 
 int main(int argc, char *argv[]) {
