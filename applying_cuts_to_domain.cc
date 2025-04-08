ec1f1d834a7b78b1f5b1a6b9e868a9b05de45a61
Jakob Niessner <jakob.niessner@uni-heidelber.de>
2025-04-08 09:00:16 +0200

    Add cut apply test

---
+++ applying_cuts_to_domain.cc
@@ -0,0 +1,124 @@
+#include "Atom_types.h"
+#include "Domain.h"
+#include "cutting_tetrahedra.h"
+#include "fileio.h"
+#include "hipSYCL/sycl/libkernel/half.hpp"
+#include "hipSYCL/sycl/libkernel/memory.hpp"
+// #include "tetraeda_type.h"
+#include <array>
+#include <cstddef>
+#include <iostream>
+#include <sycl/sycl.hpp>
+#include <sys/types.h>
+
+constexpr int Dim = 3;
+constexpr int side_length = 353;
+
+using DataType = float;
+
+using cube_edges = std::array<sycl::half, 19>;
+using cube_edges_refs =
+    std::array<sycl::atomic_ref<DataType, sycl::memory_order::relaxed,
+                                sycl::memory_scope::device>,
+               19>;
+
+using edge_ref = sycl::atomic_ref<DataType, sycl::memory_order::relaxed,
+                                  sycl::memory_scope::device>;
+
+using cube_tetrahedrons = std::array<sycl::half, 6>;
+using cube_tetrahedrons_refs = std::array<sycl::atomic<sycl::half>, 6>;
+
+struct Cutter {
+  void operator()(const domain::Grid<cube_edges_refs, Dim, side_length,
+                                     side_length, side_length> &tet_grid,
+                  const DataType *sphere_position, const DataType sphere_radius,
+                  const DataType grid_step, const std::size_t position_0,
+                  const std::size_t position_1,
+                  const std::size_t position_2) const {
+
+    Cutter_utils::vector3d point{static_cast<DataType>(position_0),
+                                 static_cast<DataType>(position_1),
+                                 static_cast<DataType>(position_2)};
+    Cutter_utils::vector3d center{sphere_position[0], sphere_position[1],
+                                  sphere_position[2]};
+    DataType radius = sphere_radius;
+    // Cutter_utils::find_polygon_cuts(
+    //     point, center, radius, grid_step,
+    //     tet_grid(position_0, position_1, position_2));
+    //  Implement the cutting logic here
+  }
+};
+
+int main(int argc, char *argv[]) {
+
+  if (argc != 5) {
+    std::cerr << "Usage: " << argv[0]
+              << " <input_pqr_file> origin_x origin_y origin_z" << std::endl;
+    return 1;
+  }
+
+  std::cout << "The size of an std::array<sycl::half, 19> is "
+            << sizeof(cube_edges) << std::endl;
+
+  std::cout << "The size of a edge_ref is " << sizeof(edge_ref) << std::endl;
+
+  sycl::gpu_selector selector;
+  sycl::queue q(selector,
+                sycl::property_list{sycl::property::queue::in_order{}});
+
+  domain::Grid<cube_edges, Dim, side_length, side_length, side_length>
+      grid_edges(Paddings::PERIODIC, q, 1);
+
+  domain::Grid<cube_edges_refs, Dim, side_length, side_length, side_length>
+      grid_edges_refs(Paddings::PERIODIC, q, 1);
+
+  domain::Grid<cube_tetrahedrons, Dim, side_length, side_length, side_length>
+      grid_tetrahedrons(Paddings::PERIODIC, q, 1);
+
+  domain::Grid<cube_tetrahedrons_refs, Dim, side_length, side_length,
+               side_length>
+      grid_tetrahedrons_refs(Paddings::PERIODIC, q, 1);
+
+  domain::Domain<Dim, side_length, side_length, side_length> values(
+      Paddings::PERIODIC, q, 1);
+
+  // std::this_thread::sleep_for(std::chrono::seconds(20));
+
+  DataType grid_step = 1.f;
+
+  std::list<Atom<DataType>> atoms;
+
+  std::array<DataType, 3> origin = {std::stof(argv[2]), std::stof(argv[3]),
+                                    std::stof(argv[4])};
+
+  std::string filename = {argv[1]};
+  read_pqr_file(filename, atoms);
+
+  std::vector<Atom<DataType>> atoms_vector;
+  atoms_vector.reserve(atoms.size());
+
+  for (auto &atom : atoms) {
+    atom.Position[0] -= origin[0];
+    atom.Position[1] -= origin[1];
+    atom.Position[2] -= origin[2];
+    atoms_vector.push_back(atom);
+  }
+
+  Cutter cutter{};
+
+  // Copy atoms to device
+  Atom<DataType> *atoms_device =
+      sycl::malloc_device<Atom<DataType>>(atoms_vector.size(), q);
+
+  q.memcpy(atoms_device, atoms_vector.data(),
+           sizeof(Atom<DataType>) * atoms_vector.size())
+      .wait();
+
+  q.parallel_for(sycl::range<1>(atoms_vector.size()), [=](sycl::id<1> i) {
+     Atom<DataType> atom = atoms_device[i];
+     cutting_cubes(cutter, grid_edges_refs, atom, grid_step);
+   }).wait();
+
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
+}
---
+++ cutting_tetrahedra.h
@@ -0,0 +1,197 @@
+#include "Atom_types.h"
+#include "Domain.h"
+#include "blas.h"
+#include "cubes_cutter.h"
+#include "hipSYCL/sycl/device_selector.hpp"
+#include "hipSYCL/sycl/libkernel/atomic_ref.hpp"
+#include "hipSYCL/sycl/libkernel/memory.hpp"
+#include "hipSYCL/sycl/queue.hpp"
+#include "scientific_quantities.h"
+#include "tetraeda_type.h"
+#include <array>
+#include <boost/container/static_vector.hpp>
+#include <cblas.h>
+#include <fstream>
+#include <iostream>
+#include <list>
+#include <sycl/sycl.hpp>
+#include <utility>
+
+#ifndef CUTTING_TETERAHEDRA_H
+#define CUTTING_TETERAHEDRA_H
+
+namespace Cutter_utils {
+
+constexpr std::size_t Dim = 3;
+
+constexpr DataType sqrt3inv = 1 / const_sqrt(3.);
+constexpr DataType sqrt2inv = 1 / const_sqrt(2.);
+
+using vector3d = blas::vector<DataType, Dim>;
+using Position3D = std::array<int, Dim>;
+
+using Face = boost::container::static_vector<vector3d, 3>;
+
+inline constexpr DataType norm(vector3d vec) {
+  DataType n = static_cast<DataType>(0);
+  for (auto i : vec)
+    n += i * i;
+
+  return std::sqrt(n);
+}
+
+template <Dimension Dim, typename DataType>
+constexpr inline DataType compute_interesect_for_no_princ(
+    std::uint8_t i, std::uint8_t distance_num, DataType &grid_step,
+    blas::vector<DataType, Dim> &point, blas::vector<DataType, Dim> &center,
+    DataType &radius) {
+
+  // std::uint8_t distance_num =
+  //     static_cast<std::uint8_t>(((i >> 1 | i << (3 - 1)) & 7));
+
+  vector3d distance{(distance_num & 1) * grid_step,
+                    ((distance_num >> 1) & 1) * grid_step,
+                    ((distance_num >> 2) & 1) * grid_step};
+
+  vector3d point_setoff =
+      point + vector3d{(i & 1) * grid_step, ((i >> 1) & 1) * grid_step,
+                       ((i >> 2) & 1) * grid_step};
+
+  return find_intersection_point_sphere<Dim>(point_setoff, distance, center,
+                                             radius);
+}
+
+inline constexpr bool in_sphere(const vector3d &point, const vector3d &center,
+                                DataType &radius) {
+  return norm(point - center) <= radius;
+}
+
+template <template <typename, std::size_t> typename container>
+void find_polygon_cuts(
+    vector3d &point, vector3d &center, DataType &radius, DataType grid_step,
+    container<sycl::atomic_ref<DataType, sycl::memory_order::relaxed,
+                               sycl::memory_scope::device>,
+              19> &lengths) {
+  // Iteration over all cubes
+  std::uint8_t points = 0;
+  for (std::uint8_t i = 0; i < 8; i++) {
+    const vector3d neighbour_point =
+        point + vector3d{(i & 1) * grid_step, ((i >> 1) & 1) * grid_step,
+                         ((i >> 2) & 1) * grid_step};
+
+    vector3d a{(i & 1) * grid_step, ((i >> 1) & 1) * grid_step,
+               ((i >> 2) & 1) * grid_step};
+
+    if (in_sphere(neighbour_point, center, radius)) {
+      points |= (1 << i);
+    }
+  }
+
+  if (points == 0 || points == 255)
+    return;
+
+  {
+    bool zero_in_sphere = (bool)(points & 1);
+    for (std::uint8_t i = 1; i <= 7; i++) {
+      if (static_cast<bool>((points >> i) & 1) == zero_in_sphere)
+        continue;
+
+      vector3d distance{(i & 1) * grid_step, ((i >> 1) & 1) * grid_step,
+                        ((i >> 2) & 1) * grid_step};
+      distance /= norm(distance);
+
+      DataType isec_p =
+          find_intersection_point_sphere<Dim>(point, distance, center, radius);
+
+      lengths[i % 7] = isec_p;
+    };
+  }
+  // Iterating over all edges connected to 7;
+  {
+    bool seven_in_sphere = (bool)((points >> 7) & 1);
+    for (std::uint8_t i = 1; i < 7; i++) {
+      std::uint8_t point_num = 7 - i;
+      // Checking whether the other point is on the other side of the surface
+      if (static_cast<bool>((points >> point_num) & 1) == seven_in_sphere)
+        continue;
+
+      vector3d distance{(i & 1) * (-grid_step), ((i >> 1) & 1) * (-grid_step),
+                        ((i >> 2) & 1) * (-grid_step)};
+
+      distance /= norm(distance);
+
+      auto point7 = point + vector3d{grid_step, grid_step, grid_step};
+
+      DataType isec_p =
+          find_intersection_point_sphere<Dim>(point7, distance, center, radius);
+      lengths[7 + i - 1] = isec_p;
+    };
+  }
+
+  // Iterating over all edges that are connected to each other
+  {
+    int edge_number = 13;
+    for (std::uint8_t i = 1; i <= 4; i *= 2) {
+
+      bool this_in_sphere = (bool)((points >> i) & 1);
+      std::uint8_t other_point_1 =
+          static_cast<std::uint8_t>(((i >> 1 | i << (3 - 1)) & 7) | i);
+
+      bool other_in_sphere = (bool)((points >> other_point_1) & 1);
+
+      std::uint8_t dir = other_point_1 - i;
+
+      if (other_in_sphere != this_in_sphere) {
+        lengths[edge_number] = compute_interesect_for_no_princ(
+            i, dir, grid_step, point, center, radius);
+      }
+
+      edge_number++;
+
+      std::uint8_t other_point_2 =
+          static_cast<std::uint8_t>(((i << 1 | i >> (3 - 1)) & 7) | i);
+
+      other_in_sphere = (bool)((points >> other_point_2) & 1);
+
+      dir = other_point_2 - i;
+
+      if (other_in_sphere != this_in_sphere) {
+        lengths[edge_number] = compute_interesect_for_no_princ(
+            i, dir, grid_step, point, center, radius);
+      }
+
+      edge_number++;
+    }
+  }
+}
+
+template <template <typename> typename container>
+void print_faces_to_ply(std::ostream &stream, container<Face> &faces) {
+  stream << "ply\n";
+  stream << "format ascii 1.0\n";
+  stream << "element vertex " << faces.size() * 3 << "\n";
+  stream << "property float x\n";
+  stream << "property float y\n";
+  stream << "property float z\n";
+  stream << "element face " << faces.size() << "\n";
+  stream << "property list uchar int vertex_index\n";
+  stream << "end_header\n";
+
+  for (auto face : faces) {
+    for (auto point : face) {
+      stream << point[0] << " " << point[1] << " " << point[2] << "\n";
+    }
+  }
+
+  int index = 0;
+  for (int i = 0; i < faces.size(); i++) {
+    stream << "3 " << index << " " << index + 1 << " " << index + 2 << "\n";
+    index += 3;
+  }
+
+  stream << std::endl;
+}
+
+} // namespace Cutter_utils
+
+#endif // CUTTING_TETERAHEDRA_H
