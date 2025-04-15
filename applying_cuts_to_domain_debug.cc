76383c618228ab11d20bc7c1b360fdce0e883f85
Jakob Niessner <jakob.niessner@uni-heidelber.de>
2025-04-15 23:56:17 +0200

    Last daily commit

--- applying_cuts_to_domain_debug.cc
+++ applying_cuts_to_domain_debug.cc
@@ -121,10 +121,10 @@ int main(int argc, char *argv[]) {
 
   atoms.push_back({.charge = 1.0f});
   atoms.back().Position = {50.5f, 50.f, 50.f};
-  atoms.back().radius = 0.5f;
-  // atoms.push_back({.charge = -1.0f});
-  // atoms.back().Position = {49.f, 50.f, 50.f};
-  // atoms.back().radius = 1.5f;
+  atoms.back().radius = 2.f;
+  //  atoms.push_back({.charge = -1.0f});
+  //  atoms.back().Position = {50.f, 50.f, 50.f};
+  //  atoms.back().radius = 0.5f;
 
   std::vector<Atom<DataType>> atoms_vector;
   atoms_vector.reserve(atoms.size());
@@ -205,19 +205,19 @@ int main(int argc, char *argv[]) {
         //  if (cut_cells < 5)
         //    continue;
 
-        for (int l = 0; l < 21; l++) {
-          std::cout << static_cast<DataType>(grid_value_span[l]) /
-                           static_cast<DataType>(
-                               std::numeric_limits<std::uint32_t>::max())
-                    << " ";
-        }
+        //    for (int l = 0; l < 21; l++) {
+        //      std::cout << static_cast<DataType>(grid_value_span[l]) /
+        //                       static_cast<DataType>(
+        //                           std::numeric_limits<std::uint32_t>::max())
+        //                << " ";
+        //    }
 
-        std::cout << std::endl;
+        // std::cout << std::endl;
 
         compute_faces(points, grid_step, point, grid_value_span, faces);
 
-        std::cout << i << " " << j << " " << k << " "
-                  << static_cast<int>(points) << std::endl;
+        //  std::cout << i << " " << j << " " << k << " "
+        //            << static_cast<int>(points) << std::endl;
 
         //  if (5 == cut_cells)
         //    goto endloop;
